b32c0d542f1d961a83dd3323b638fce10092ffd1
ManasiKhot <mansikhotd@gmail.com>
2025-05-02 00:19:23 +0530

    Added delete  functionality to Todo app

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
-import React ,{useRef, useState} from 'react'
+import React ,{useRef, useState,useEffect} from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from "./TodoItems"
 
 function Todo() {
 
-  const [TodoList , setTodoList] = useState([]);
+  const [TodoList , setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")):[]);
 
 const inputRef = useRef();
 function add(){
@@ -31,6 +31,21 @@ function deleteTodo(id){
   })
 }
 
+function toggle(id){
+  setTodoList((prevTodos)=>{
+    return prevTodos.map((todo)=>{
+      if(todo.id === id){
+        return {...todo, isComplete: !todo.isComplete }
+      }
+      return todo;
+    })
+  })
+}
+
+useEffect(()=>{
+  localStorage.setItem("todos",JSON.stringify(TodoList));
+} ,[TodoList])
+
     return(
         <div className = 'bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl shadow-2xl'>
           
@@ -43,14 +58,14 @@ function deleteTodo(id){
            {/* ---- input box -----*/}
            <div className = 'flex items-centre my-7 bg-gray-200 rounded-full'>
             <input ref ={inputRef}  className = 'bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder: text-slate-600'type = "text" placeholder = 'Add your task'/>
-            <button onClick ={add}className = 'border-none rounded-md bg-green-500 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+            <button onClick ={add}className = 'border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
            </div>
 
            {/* ---- Todo - list -----*/}
            <div>
 
             {TodoList.map((item,index)=>{
-              return <TodoItems key ={index} text ={item.text} id ={item.id} isComplete ={item.isComplete}/>
+              return <TodoItems key ={index} text ={item.text} id ={item.id} isComplete ={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
             })}
             
            </div>
